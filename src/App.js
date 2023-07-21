a710b140ffde858a182155a757886535278ec6ff
Vaibhav9401 <vaibhav@kooapp.com>
2023-07-21 23:05:26 +0530

    Updated dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Filters() {
+    return (
+        <div className="filters">
+            <button>All</button>
+            <button>Organic</button>
+            <button>High Potential</button>
+            <button>Eminent</button>
+            {/* Add other filters here */}
+        </div>
+    );
 }
 
-export default App;
+function Card() {
+    return (
+        <div className="card">
+            <img src="https://via.placeholder.com/50" alt="Profile" />
+            <div>Name</div>
+            <div>Story content</div>
+            <button>Approve</button>
+            <button>Reject</button>
+            <input type="checkbox" /> Mark as High Potential
+        </div>
+    );
+}
+
+function Dashboard() {
+    return (
+        <div className="container">
+            <div className="heading">Stories Dashboard</div>
+            <Filters />
+            <div className="content">
+                <Card />
+                {/* Add more cards here */}
+            </div>
+        </div>
+    );
+}
+
+ReactDOM.render(<Dashboard />, document.getElementById('root'));
